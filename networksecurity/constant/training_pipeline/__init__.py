90cedac299bb6eedf8d1929d243eee47434a13a6
notzainab12 <zainab122526@gmail.com>
2025-12-31 18:29:42 +0530

    Data ingestion component

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -3,15 +3,13 @@ import sys
 import numpy as np
 import pandas as pd
 
-
-
 """
 defining common constant variable for training pipeline
 """
 TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
-FILE_NAME: str = "phishingData.csv"
+FILE_NAME: str = "phisingData.csv"
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
